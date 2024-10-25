49aeaeb62f910d3f96085fdf2b2e29340bb01e7e
Javier Molina <javier.molina@mail.udp.cl>
2024-10-25 02:20:04 -0300

    Update users.py

--- app/routes/users.py
+++ app/routes/users.py
@@ -3,7 +3,8 @@ from fastapi.responses import JSONResponse
 from sqlmodel import Session, select
 from ..core.database import get_session
 from ..models.user import User
-from ..schemas.user import UserResponseSchema
+from ..schemas.user import UserResponseSchema, UserCredentialsSchema
+from ..services.password_service import hash_password, verify_password 
 import urllib.parse
 
 router = APIRouter()
@@ -11,40 +12,64 @@ router = APIRouter()
 @router.post("/", response_model=UserResponseSchema)
 def create_user(user: User, session: Session = Depends(get_session)):
     
-    statement = select(User).where(User.email == user.email)
-    result = session.exec(statement)
+    try:
+        statement = select(User).where(User.email == user.email)
+        result = session.exec(statement)
+    except Exception as e:
+        print(f"An error has ocurred: {e}")
 
     if result.first():
         raise HTTPException(status_code=409, detail="Email is already in use.")
 
     try:
+        user.password = hash_password(user.password)
         session.add(user)
         session.commit()
         session.refresh(user)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error has ocurred: {e}")
+        print(f"An error has ocurred: {e}")
     return user
 
 @router.get("/id/{user_id}", response_model=UserResponseSchema)
 def read_user_by_id(user_id: int, session: Session = Depends(get_session)):
     try:  
         user = session.get(User, user_id)
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
-        return user
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error has ocurred: {e}")
+        print(f"An error has ocurred: {e}")
+        
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return user
 
 @router.get("/email/{user_email}", response_model=UserResponseSchema)
 def read_user_by_email(user_email: str, session: Session = Depends(get_session)):
     try:
         user_email = urllib.parse.unquote(user_email)
+
         statement = select(User).where(User.email == user_email)
         result = session.exec(statement)
         user = result.first()
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
-        else:
-            return user
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error has ocurred: {e}")
+        print(f"An error has ocurred: {e}")
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return user
+
+# For now this route will respond with UserResponseSchema.
+# This should respond with a jwt or something like that.
+@router.post("/login/", response_model=UserResponseSchema)
+def login(user_credentials: UserCredentialsSchema, session: Session = Depends(get_session)):
+    try:
+        statement = select(User).where(User.email == user_credentials.email)
+        result = session.exec(statement)
+        user = result.first()
+    except Exception as e:
+        print(f"An error has ocurred: {e}")
+
+    if not user or not verify_password(user_credentials.password, user.password):
+        raise HTTPException(status_code=401, detail="Wrong credetials.")
+    
+    return user
