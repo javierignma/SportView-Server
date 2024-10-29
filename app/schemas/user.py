b9f13c012802ef5247b00599609d8e56a72ec430
Javier Molina <javier.molina@mail.udp.cl>
2024-10-29 00:03:39 -0300

    add access and refresh token to user schema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -5,6 +5,8 @@ class UserResponseSchema(SQLModel, table=False):
     email: str
     first_name: Optional[str]
     last_name: Optional[str]
+    access_token: str
+    refresh_token: str
 
 class UserCredentialsSchema(SQLModel, table=False):
     email: str
