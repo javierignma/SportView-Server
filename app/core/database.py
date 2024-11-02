f0570e17417c6e8fc5a053801f7cc531ab35de17
Javier Molina <javier.molina@mail.udp.cl>
2024-11-02 02:15:15 -0300

    Update database.py

--- app/core/database.py
+++ app/core/database.py
@@ -1,8 +1,6 @@
 from sqlmodel import SQLModel, create_engine, Session
 from .config import DATABASE_URL
 
-print(f"[core.database] DATABASE_URL: {DATABASE_URL}")
-
 engine = create_engine(DATABASE_URL)
 
 def create_db_and_tables():
