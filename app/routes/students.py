7ff845ae86352eeef4eecf9532c4c26b7fa07337
Javier Molina <javier.molina@mail.udp.cl>
2024-11-08 04:55:17 -0300

    change delete student logic

--- app/routes/students.py
+++ app/routes/students.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 from sqlmodel import Session, select, update
 import urllib
 
+from app.models.attendance import Attendance
 from app.services.jwt_service import token_verifier
 from ..core.database import get_session
 from ..models.student import Student, StudentProgress
@@ -123,6 +124,22 @@ def post_student_progress(student_progress: StudentProgress, session: Session = 
 
 @router.delete("/delete/id/{student_id}")
 def delete_student(student_id: int, session: Session = Depends(get_session), dependencies = [Depends(token_verifier)]):
+
+    try:
+        statement = select(Attendance).where(Attendance.student_id == student_id)
+        result = session.exec(statement)
+        attendance = result.first()
+
+        if attendance:
+            session.delete(attendance)
+            session.commit()
+
+    except Exception as e:
+        print(f"An error has ocurred: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error")
+    
+    # Do the same with StudentProgress
+
     try:
         statement = select(Student).where(Student.id == student_id)
         result = session.exec(statement)
